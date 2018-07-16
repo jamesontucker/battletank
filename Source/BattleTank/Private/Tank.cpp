5b44d6ef8a95b0cbc3028b4b267d11dd1b90bbba
Jameson <jamesoncoletucker@yahoo.com>
2018-07-16 00:04:56 -0400

    Added tank aiming component, updated tank logging, misc clean up

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -9,6 +9,13 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// No need to protect points as added at const
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+}
+
+void ATank::SetBarrelReference(UStaticMeshComponent* BarrelToSet) 
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
 }
 
 // Called when the game starts or when spawned
@@ -32,3 +39,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 }
 
+void ATank::AimAt(FVector HitLocation) {
+	TankAimingComponent->AimAt(HitLocation);
+}
--- Source/BattleTank/Public/Tank.h
+++ Source/BattleTank/Public/Tank.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
 UCLASS()
@@ -12,14 +13,23 @@ class BATTLETANK_API ATank : public APawn
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ATank();
+
+	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+private:	
+
+	// Sets default values for this pawn's properties
+	ATank();
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
