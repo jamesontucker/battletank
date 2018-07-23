6a4b0d2bac041dd9e7271c15558d41355c8d8635
Jameson <jamesoncoletucker@yahoo.com>
2018-07-22 20:03:31 -0400

    Updated SetThrottle logic for tank track class

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -8,6 +8,9 @@ void UTankTrack::SetThrottle(float Throttle)
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s: throttle: %f"), *Name, Throttle);
 
-	// TODO clamp actual throttle value so plauer can't over-drive
+	// TODO clamp actual throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
-
