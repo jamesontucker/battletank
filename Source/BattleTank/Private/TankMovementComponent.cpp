5e8bdce212d29e7e05c9fe91def0363b7a19ed5f
Jameson <jamesoncoletucker@yahoo.com>
2018-07-23 20:57:47 -0400

    Updated gamepad tank controls

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -5,16 +5,26 @@
 
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
-
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
+	if (!LeftTrack || !RightTrack) { return; }
+
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
+
+	// TODO prevent double-speed due to dual control use
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 
 	// TODO prevent double-speed due to dual control use
 }
