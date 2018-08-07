4d4926831ecb4bd837b59dcc65ac84742ce655b5
Jameson <jamesoncoletucker@yahoo.com>
2018-08-06 20:03:22 -0400

    Removed AI controller ensure methods and fixed projecitle placement for mortar

--- Source/BattleTank/Private/TankAIController.cpp
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -17,7 +17,7 @@ void ATankAIController::SetPawn(APawn* InPawn)
 	if (InPawn)
 	{
 		auto PossessedTank = Cast<ATank>(InPawn);
-		if (!ensure(PossessedTank)) { return; }
+		if (!(PossessedTank)) { return; }
 
 		// Subscribe our local method to the tank's death event
 		PossessedTank->OnDeath.AddUniqueDynamic(this, &ATankAIController::OnPossessedTankDeath);
@@ -31,7 +31,7 @@ void ATankAIController::Tick(float DeltaTime)
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ControlledTank = GetPawn();
 
-	if (!ensure(PlayerTank && ControlledTank)) { return; }
+	if (!(PlayerTank && ControlledTank)) { return; }
 
 	//Move towards the player
 	MoveToActor(PlayerTank, AcceptanceRadius);
